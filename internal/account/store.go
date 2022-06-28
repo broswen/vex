af4420f2e2d22d2af6bb0c3bf1eff2be76afb7ac
Brad <broswen@gmail.com>
2022-06-27 19:07:40 -0500

    fix account description

--- internal/account/store.go
+++ internal/account/store.go
@@ -21,21 +21,21 @@ func NewPostgresStore(database *db.Database) (*Store, error) {
 }
 
 func (store *Store) Insert(ctx context.Context, a *Account) error {
-	err := db.PgError(store.db.QueryRow(ctx, `INSERT INTO account (account_name) VALUES ($1) RETURNING id, account_name;`,
-		a.Name).Scan(&a.ID, &a.Name))
+	err := db.PgError(store.db.QueryRow(ctx, `INSERT INTO account (account_name, account_description) VALUES ($1, $2) RETURNING id, account_name, account_description;`,
+		a.Name, a.Description).Scan(&a.ID, &a.Name, &a.Description))
 	return err
 }
 
 func (store *Store) Update(ctx context.Context, a *Account) error {
-	err := db.PgError(store.db.QueryRow(ctx, `UPDATE account SET account_name = $2 WHERE id = $1 RETURNING id, account_name;`,
-		a.ID, a.Name).Scan(&a.ID, &a.Name))
+	err := db.PgError(store.db.QueryRow(ctx, `UPDATE account SET account_name = $2, account_description = $3 WHERE id = $1 RETURNING id, account_name, account_description;`,
+		a.ID, a.Name, a.Description).Scan(&a.ID, &a.Name, &a.Description))
 	return err
 }
 
 func (store *Store) Get(ctx context.Context, id string) (*Account, error) {
 	a := &Account{}
-	err := db.PgError(store.db.QueryRow(ctx, `SELECT id, account_name FROM account WHERE id = $1;`,
-		id).Scan(&a.ID, &a.Name))
+	err := db.PgError(store.db.QueryRow(ctx, `SELECT id, account_name, account_description FROM account WHERE id = $1;`,
+		id).Scan(&a.ID, &a.Name, &a.Description))
 	return a, err
 }
 
@@ -44,8 +44,3 @@ func (store *Store) Delete(ctx context.Context, id string) error {
 	err = db.PgError(err)
 	return err
 }
-
-//POST create new token with read or readonly permissions, returns token secret
-//PUT regenerate token secret
-//GET list all token ids for an account and readonly status, doesn't return token secret
-//DELETE delete token
